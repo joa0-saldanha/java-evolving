7df8139788c86c8c98117cf849a01e6ff2522509
joaoaos <joaoaosaldanha@gmail.com>
2022-08-19 12:49:33 -0300

    torre

---
+++ torre.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+import java.math.*;
+import java.util.ArrayList;
+
+public class Main {
+  public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
+    
+    // FUNÇAO MATRIZ 
+    int n = sc.nextInt();
+    int M[][] = new int[n][n];
+    for (int i = 0; i < n; i++)
+      for (int j = 0; j < n; j++)
+        M[i][j] = sc.nextInt();
+
+    // Variaveis
+    int soma=0, maior=0;
+    int d=0, e=0, c=0, b=0;
+
+    // Resolução
+    for (int i=0; i<n; i++) {
+      for (int j=0; j<n; j++) {
+        int num= M[i][j];
+        for (int k=j+1; k<n; k++) {
+          int nun=M[i][k];
+          d += nun;
+        }
+        for (int l=j-1; l>=0; l--) {
+          int nun=M[i][l];
+          e += nun;
+        }
+        for (int x=i+1; x<n; x++) {
+          int nun=M[x][j];
+          b += nun;
+        }
+        for (int y=i-1; y>=0; y--) {
+          int nun=M[y][j];
+          c += nun;
+        }
+        soma = d+e+b+c;
+        if (soma>maior) {
+          maior=soma;
+        }
+        d=0;
+        e=0;
+        c=0;
+        b=0;
+      }
+    }
+    System.out.println(maior);
+  }
+}
+
+
+
+
+
+
